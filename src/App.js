306db3d4269ba0a7e31368cb6fd8e7a0a74c57cb
NiuRuslan <niuruslan@gmail.com>
2020-04-07 17:39:26 +0300

    task 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GitHubInfo />
       </header>
     </div>
   );
 }
+
+const GitHubInfo = () => {
+  const [repos, setRepo] = useState([]);
+
+  useEffect(() => {
+    const monthAgo = new Date(Date.now() - (1000 * 3600 * 24 * 31)).toISOString().slice(0, 10);
+
+    fetch(`https://api.github.com/search/repositories?q=+language:javascript+created%3A%3E${monthAgo}&sort=stars`)
+      .then((res) => res.json())
+      .then((data) => setRepo(data.items));
+    return () => {
+    };
+  }, []);
+
+  function dateFormatter(oldDate) {
+    return new Date(oldDate).toLocaleDateString();
+  }
+
+
+  const list = repos.map((repo) => (
+    <div key={repo.id}>
+      <a href={repo.html_url}>
+        {repo.name}
+      </a>
+      {' - Stars: '}
+      {repo.stargazers_count}
+      {' - created at: '}
+      {dateFormatter(repo.created_at)}
+    </div>
+  ));
+
+  return (
+    <div>
+      {list}
+    </div>
+  );
+};
 
 export default App;
