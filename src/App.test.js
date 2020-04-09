6e277831870929971954ec8affd826b178f7bc2c
NiuRuslan <niuruslan@gmail.com>
2020-04-09 19:15:52 +0300

    tasks 1 2 3 5 7 complete (4 - manual, 6 - by loader)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './containers/App';
 
-test('renders learn react link', () => {
+test('renders search bar', () => {
+  const { getByPlaceholderText } = render(<App />);
+  const linkElement = getByPlaceholderText(/Search by repo's name/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders filter', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Select license to filter/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders loader', () => {
+  const { getByAltText } = render(<App />);
+  const linkElement = getByAltText(/loading/i);
   expect(linkElement).toBeInTheDocument();
 });
